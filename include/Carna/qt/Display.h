f2ee5bca2b138f55de62fde6423be9975b266899
Leonid Kostrykin <void@evoid.de>
2015-03-13 15:27:18 +0100

    implemented Display

--- include/Carna/qt/Display.h
+++ include/Carna/qt/Display.h
@@ -14,8 +14,12 @@
 
 #include <Carna/qt/CarnaQt.h>
 #include <Carna/base/noncopyable.h>
+#include <Carna/base/Aggregation.h>
 #include <QGLWidget>
 #include <memory>
+
+class QMouseEvent;
+class QWheelEvent;
 
 /** \file   Display.h
   * \brief  Defines \ref Carna::qt::Display.
@@ -33,6 +37,18 @@ namespace qt
 // Display
 // ----------------------------------------------------------------------------------
 
+/** \brief
+  * Abstract class that encapsulates a `base::FrameRenderer` and a `base::GLContext`.
+  *
+  * Override \ref setupRenderer in order to implement.
+  *
+  * \note
+  * You cannot put a frame renderer from one display to another. The reason for this
+  * strong coupling is that `base::FrameRenderer` requires *one* particular OpenGL
+  * context, whose lifetime is coupled to the timetime of the hosting \c QGLWidget.
+  * So instead of moving the renderer, just put the display to another location in
+  * your GUI.
+  */
 class CARNAQT_LIB Display : public QGLWidget
 {
 
@@ -43,36 +59,74 @@ class CARNAQT_LIB Display : public QGLWidget
 
 public:
 
+    /** \brief
+      * Holds the default radians per pixel ratio used for camera rotation.
+      */
+    const static float DEFAULT_ROTATION_SPEED;
+    
+    const static float DEFAULT_AXIAL_MOVEMENT_SPEED;
+    
+    enum ViewportMode
+    {
+        fitSquare,
+        fitFrame,
+        fitAuto
+    };
+
     explicit Display( QWidget* parent = nullptr );
     
     virtual ~Display();
     
+    void setViewportMode( ViewportMode );
+    
+    void setRotationSpeed( float radiansPerPixel );
+    
+    void setAxialMovementSpeed( float axialMovementSpeed );
+    
     void setCamera( base::Camera& );
+    
+    bool hasCamera() const;
     
     base::Camera& camera();
     
     const base::Camera& camera() const;
     
+    void setCameraControl( base::Aggregation< base::CameraControl >* camControl );
+    
+    bool hasCameraControl() const;
+    
+    base::CameraControl& cameraControl();
+    
+    const base::CameraControl& cameraControl() const;
+    
+    void setProjectionControl( base::Aggregation< base::ProjectionControl >* projControl );
+    
+    bool hasProjectionControl() const;
+    
+    base::ProjectionControl& projectionControl();
+    
+    const base::ProjectionControl& projectionControl() const;
+    
 protected:
 
     /** \brief
-      * Creates the renderer that will be responsible for rendering frames within
-      * this display for the lifetime of the display.
-      *
-      * \note
-      * You cannot put a frame renderer from one display to another. The reason for
-      * this strong coupling is that \c FrameRenderer requires *one* particular GL
-      * context, whose lifetime is coupled to the timetime of the hosting
-      * \c QGLWidget. So instead of moving the renderer, just put the display to
-      * another location in your GUI.
+      * Configures the \a frameRenderer.
       */
-    virtual base::FrameRenderer* createRenderer( base::GLContext&, unsigned int width, unsigned int height ) = 0;
+    virtual void setupRenderer( base::FrameRenderer& frameRenderer ) = 0;
     
     virtual void initializeGL() override;
     
     virtual void resizeGL( int w, int h ) override;
     
     virtual void paintGL() override;
+    
+    virtual void mousePressEvent( QMouseEvent* ev ) override;
+    
+    virtual void mouseMoveEvent( QMouseEvent* ev ) override;
+    
+    virtual void mouseReleaseEvent( QMouseEvent* ev ) override;
+    
+    virtual void wheelEvent( QWheelEvent* ev ) override;
 
 }; // Display
 
--- src/qt/Display.cpp
+++ src/qt/Display.cpp
@@ -11,10 +11,16 @@
 
 #include <Carna/qt/Display.h>
 #include <Carna/base/FrameRenderer.h>
+#include <Carna/base/SpatialMovement.h>
 #include <Carna/base/GLContext.h>
 #include <Carna/base/Camera.h>
 #include <Carna/base/Log.h>
+#include <Carna/base/ProjectionControl.h>
+#include <Carna/base/CameraControl.h>
+#include <Carna/presets/MeshColorCodingStage.h>
 #include <QGLContext>
+#include <QMouseEvent>
+#include <QWheelEvent>
 
 namespace Carna
 {
@@ -36,16 +42,95 @@ struct Display::Details
 
     std::unique_ptr< GLContext > glc;
     std::unique_ptr< base::FrameRenderer > renderer;
+    bool glInitializationFinished;
+    
+    ViewportMode vpMode;
     
     base::Camera* cam;
     base::Node* root;
+    std::unique_ptr< base::Aggregation< base::CameraControl > > camControl;
+    std::unique_ptr< base::Aggregation< base::ProjectionControl > > projControl;
+
+    bool mouseInteraction;
+    QPoint mousepos;
+    float radiansPerPixel;
+    float axialMovementSpeed;
+    
+    presets::MeshColorCodingStage* mccs;
+    std::unique_ptr< base::SpatialMovement > spatialMovement;
+    
+    void updateProjection( Display& );
+    bool fitSquare() const;
 };
 
 
 Display::Details::Details()
-    : cam( nullptr )
+    : glInitializationFinished( false )
+    , vpMode( fitAuto )
+    , cam( nullptr )
     , root( nullptr )
+    , radiansPerPixel( DEFAULT_ROTATION_SPEED )
+    , axialMovementSpeed( DEFAULT_AXIAL_MOVEMENT_SPEED )
+    , mccs( nullptr )
 {
+}
+
+
+void Display::Details::updateProjection( Display& display )
+{
+    if( display.hasCamera() && display.hasProjectionControl() && display.width() > 0 && display.height() > 0 )
+    {
+        const unsigned int width  = static_cast< unsigned int >( display. width() );
+        const unsigned int height = static_cast< unsigned int >( display.height() );
+        
+        /* Update projection parameters.
+         */
+        if( fitSquare() )
+        {
+            const unsigned int vpSize = std::min( width, height );
+            display.projectionControl().setViewportWidth ( vpSize );
+            display.projectionControl().setViewportHeight( vpSize );
+        }
+        else
+        {
+            display.projectionControl().setViewportWidth (  width );
+            display.projectionControl().setViewportHeight( height );
+        }
+        
+        /* Fetch new projection matrix.
+         */
+        base::math::Matrix4f projection;
+        display.projectionControl().updateProjection( projection );
+        display.camera().setProjection( projection );
+        
+        /* Redraw if required.
+         */
+        if( glInitializationFinished )
+        {
+            display.updateGL();
+        }
+    }
+}
+
+
+bool Display::Details::fitSquare() const
+{
+    switch( vpMode )
+    {
+    
+    case Display::fitSquare:
+        return true;
+        
+    case Display::fitFrame:
+        return false;
+        
+    case Display::fitAuto:
+        return projControl.get() != nullptr && projControl->get() != nullptr;
+        
+    default:
+        CARNA_FAIL( "Unknown ViewportMode value." );
+        
+    }
 }
 
 
@@ -53,6 +138,10 @@ Display::Details::Details()
 // ----------------------------------------------------------------------------------
 // Display
 // ----------------------------------------------------------------------------------
+
+const float Display::DEFAULT_ROTATION_SPEED       = 1e-2f;
+const float Display::DEFAULT_AXIAL_MOVEMENT_SPEED = 1e+0f;
+
 
 Display::Display( QWidget* parent )
     : QGLWidget( parent )
@@ -70,13 +159,37 @@ Display::~Display()
 }
 
 
+void Display::setViewportMode( ViewportMode vpMode )
+{
+    if( vpMode != pimpl->vpMode )
+    {
+        pimpl->vpMode = vpMode;
+        if( pimpl->renderer.get() != nullptr )
+        {
+            resizeGL( width(), height() );
+        }
+    }
+}
+
+
 void Display::setCamera( base::Camera& cam )
 {
     pimpl->cam = &cam;
+    pimpl->updateProjection( *this );
+    if( hasCameraControl() )
+    {
+        cameraControl().setCamera( cam );
+    }
     
     /* We will search the root node the first time we render the frame.
      */
     pimpl->root = nullptr;
+}
+
+
+bool Display::hasCamera() const
+{
+    return pimpl->cam != nullptr;
 }
 
 
@@ -106,13 +219,16 @@ void Display::resizeGL( int w, int h )
     const unsigned int height = static_cast< unsigned int >( h );
     if( pimpl->renderer == nullptr )
     {
-        base::FrameRenderer* const renderer = createRenderer( *pimpl->glc, width, height );
-        CARNA_ASSERT( renderer != nullptr );
-        pimpl->renderer.reset( renderer );
+        pimpl->renderer.reset( new base::FrameRenderer( *pimpl->glc, width, height, pimpl->fitSquare() ) );
+        setupRenderer( *pimpl->renderer );
+        pimpl->mccs = pimpl->renderer->findStage< presets::MeshColorCodingStage >().get();
+        pimpl->updateProjection( *this );
+        pimpl->glInitializationFinished = true;
     }
     else
     {
-        pimpl->renderer->reshape( width, height );
+        pimpl->renderer->reshape( width, height, pimpl->fitSquare() );
+        pimpl->updateProjection( *this );
     }
 }
 
@@ -133,6 +249,160 @@ void Display::paintGL()
         }
         pimpl->renderer->render( *pimpl->cam, *pimpl->root );
     }
+}
+
+
+void Display::setRotationSpeed( float radiansPerPixel )
+{
+    pimpl->radiansPerPixel = radiansPerPixel;
+}
+
+
+void Display::setAxialMovementSpeed( float axialMovementSpeed )
+{
+    pimpl->axialMovementSpeed = axialMovementSpeed;
+}
+
+
+void Display::mousePressEvent( QMouseEvent* ev )
+{
+    /* First, try to pick object at clicked location.
+     */
+    const base::Geometry* picked = nullptr;
+    if( pimpl->mccs != nullptr )
+    {
+        picked = pimpl->mccs->pick( ev->x(), ev->y() ).get();
+    }
+    
+    /* Initiate camera rotation if nothing was picked.
+     */
+    if( picked == nullptr )
+    {
+        pimpl->mousepos = ev->pos();
+        pimpl->mouseInteraction = true;
+        ev->accept();
+    }
+    else
+    if( pimpl->renderer != nullptr && hasCamera() && hasCameraControl() )
+    {
+        /* Picking was successful! Initiate spatial movement.
+         */
+        base::Geometry& pickedGeometry = const_cast< base::Geometry& >( *picked );
+        pimpl->spatialMovement.reset
+            ( new base::SpatialMovement( pickedGeometry, ev->x(), ev->y(), pimpl->renderer->viewport(), *pimpl->cam ) );
+        pimpl->mouseInteraction = true;
+        ev->accept();
+    }
+}
+
+
+void Display::mouseMoveEvent( QMouseEvent* ev )
+{
+    if( pimpl->mouseInteraction )
+    {
+        if( pimpl->spatialMovement.get() == nullptr && hasCamera() && hasCameraControl() )
+        {
+            /* Camera rotation is going on.
+             */
+            const int dx = ( ev->x() - pimpl->mousepos.x() );
+            const int dy = ( ev->y() - pimpl->mousepos.y() );
+            pimpl->mousepos = ev->pos();
+
+            if( dx != 0 || dy != 0 )
+            {
+                cameraControl().rotateHorizontally( dx * pimpl->radiansPerPixel );
+                cameraControl().rotateVertically  ( dx * pimpl->radiansPerPixel );
+                updateGL();
+                ev->accept();
+            }
+        }
+        else
+        if( pimpl->spatialMovement.get() != nullptr )
+        {
+            /* Spatial movement is going on. Update it.
+             */
+            if( pimpl->spatialMovement->update( ev->x(), ev->y() ) )
+            {
+                updateGL();
+                ev->accept();
+            }
+        }
+    }
+}
+
+
+void Display::mouseReleaseEvent( QMouseEvent* ev )
+{
+    pimpl->mouseInteraction = false;
+    pimpl->spatialMovement.reset();
+    ev->accept();
+}
+
+
+void Display::wheelEvent( QWheelEvent* ev )
+{
+    if( hasCamera() && hasCameraControl() )
+    {
+        cameraControl().moveAxially( ev->delta() * pimpl->axialMovementSpeed );
+        ev->accept();
+    }
+}
+    
+    
+void Display::setCameraControl( base::Aggregation< base::CameraControl >* camControl )
+{
+    pimpl->camControl.reset( camControl );
+    if( hasCamera() )
+    {
+        cameraControl().setCamera( *pimpl->cam );
+    }
+}
+
+
+bool Display::hasCameraControl() const
+{
+    return pimpl->camControl.get() != nullptr && pimpl->camControl->get() != nullptr;
+}
+
+
+base::CameraControl& Display::cameraControl()
+{
+    CARNA_ASSERT( hasCameraControl() );
+    return **pimpl->camControl;
+}
+
+
+const base::CameraControl& Display::cameraControl() const
+{
+    CARNA_ASSERT( hasCameraControl() );
+    return **pimpl->camControl;
+}
+    
+    
+void Display::setProjectionControl( base::Aggregation< base::ProjectionControl >* projControl )
+{
+    pimpl->projControl.reset( projControl );
+    pimpl->updateProjection( *this );
+}
+
+
+bool Display::hasProjectionControl() const
+{
+    return pimpl->projControl.get() != nullptr && pimpl->projControl->get() != nullptr;
+}
+
+
+base::ProjectionControl& Display::projectionControl()
+{
+    CARNA_ASSERT( hasProjectionControl() );
+    return **pimpl->projControl;
+}
+
+
+const base::ProjectionControl& Display::projectionControl() const
+{
+    CARNA_ASSERT( hasProjectionControl() );
+    return **pimpl->projControl;
 }
 
 
