cf71b569071b0f972d29c8a28cfb5708cd170c88
Leonid Kostrykin <void@evoid.de>
2015-04-03 14:54:13 +0200

    Moved sample rate configuration from DVRControl to VolumeRenderingControl.

---
+++ include/Carna/qt/VolumeRenderingControl.h
@@ -0,0 +1,74 @@
+/*
+ *  Copyright (C) 2010 - 2014 Leonid Kostrykin
+ *
+ *  Chair of Medical Engineering (mediTEC)
+ *  RWTH Aachen University
+ *  Pauwelsstr. 20
+ *  52074 Aachen
+ *  Germany
+ *
+ */
+
+#ifndef VOLUMERENDERINGCONTROL_H_0874895466
+#define VOLUMERENDERINGCONTROL_H_0874895466
+
+/** \file   VolumeRenderingControl.h
+  * \brief  Defines \ref Carna::qt::VolumeRenderingControl
+  */
+
+#include <Carna/qt/CarnaQt.h>
+#include <Carna/qt/RenderStageControl.h>
+#include <Carna/base/noncopyable.h>
+#include <QWidget>
+
+class QSpinBox;
+
+namespace Carna
+{
+
+namespace qt
+{
+
+
+
+// ----------------------------------------------------------------------------------
+// VolumeRenderingControl
+// ----------------------------------------------------------------------------------
+
+class CARNAQT_LIB VolumeRenderingControl : public QWidget, public RenderStageControl
+{
+
+    Q_OBJECT
+    NON_COPYABLE
+
+public:
+
+    /** \brief
+      * Instantiates.
+      */
+    explicit VolumeRenderingControl( presets::VolumeRenderingStage&, QWidget* parent = nullptr );
+
+    /** \brief
+      * Deletes.
+      */
+    virtual ~VolumeRenderingControl();
+
+    presets::VolumeRenderingStage& stage;
+    
+public slots:
+
+    void setSampleRate( int samplesPerPixel );
+
+protected:
+
+    QSpinBox* const sbSampleRate;
+
+}; // VolumeRenderingControl
+
+
+
+}  // namespace Carna :: qt
+
+}  // namespace Carna
+
+#endif // VOLUMERENDERINGCONTROL_H_0874895466
---
+++ src/qt/VolumeRenderingControl.cpp
@@ -0,0 +1,62 @@
+/*
+ *  Copyright (C) 2010 - 2014 Leonid Kostrykin
+ *
+ *  Chair of Medical Engineering (mediTEC)
+ *  RWTH Aachen University
+ *  Pauwelsstr. 20
+ *  52074 Aachen
+ *  Germany
+ *
+ */
+
+#include <Carna/qt/VolumeRenderingControl.h>
+#include <Carna/presets/VolumeRenderingStage.h>
+#include <QSpinBox>
+
+namespace Carna
+{
+
+namespace qt
+{
+
+
+
+// ----------------------------------------------------------------------------------
+// VolumeRenderingControl
+// ----------------------------------------------------------------------------------
+
+VolumeRenderingControl::VolumeRenderingControl( presets::VolumeRenderingStage& stage, QWidget* parent )
+    : QWidget( parent )
+    , RenderStageControl( stage )
+    , stage( stage )
+    , sbSampleRate( new QSpinBox() )
+{
+    sbSampleRate->setMinimum( 10 );
+    sbSampleRate->setMaximum( 10000 );
+    sbSampleRate->setSingleStep( 10 );
+    sbSampleRate->setValue( stage.sampleRate() );
+    
+    connect( sbSampleRate, SIGNAL( valueChanged( int ) ), this, SLOT( setSampleRate( int ) ) );
+}
+
+
+VolumeRenderingControl::~VolumeRenderingControl()
+{
+}
+
+
+void VolumeRenderingControl::setSampleRate( int samplesPerPixel )
+{
+    samplesPerPixel = base::math::clamp< int >( samplesPerPixel, sbSampleRate->minimum(), sbSampleRate->maximum() );
+    if( samplesPerPixel != stage.sampleRate() )
+    {
+        stage.setSampleRate( samplesPerPixel );
+        RenderStageControl::invalidate();
+    }
+}
+
+
+
+}  // namespace Carna :: qt
+
+}  // namespace Carna
